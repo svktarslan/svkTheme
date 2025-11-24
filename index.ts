ebaaae2723161acce64248427df3c90aef111e6f
şevket arslan <svktarslan@sevket-MacBook-Pro-2.local>
2025-11-25 01:11:18 +0300

    version

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-export * from './components';
-export * from './hooks/useTheme';
-export { default as layout } from './theme/layout';
-export { ThemeProvider } from './hooks/ThemeProvider';
-
-
+export * from "./components";
+export * from "./hooks/useTheme";
+export { default as layout } from "./theme/layout";
+export { ThemeProvider, useThemeMode } from "./hooks/ThemeProvider";
